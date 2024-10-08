7977650ce962655cd09b1dc2db551c27e6917c6f
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-08 16:49:37 +0200

    bg color

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('My First App'),
       ),
@@ -11,7 +12,7 @@ void main() {
             style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 98, 113, 125),
+                color: Color.fromARGB(255, 104, 136, 162),
                 fontStyle: FontStyle.italic)),
       ),
     ),
