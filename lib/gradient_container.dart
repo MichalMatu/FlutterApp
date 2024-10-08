a5f4af55d3e3d6b887c1deed68e09c248bcb8ea7
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-08 22:12:29 +0200

    separate file

---
+++ lib/gradient_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 86, 176, 250),
+            Color.fromARGB(255, 49, 104, 172),
+            Color.fromARGB(255, 27, 73, 138),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 56, 58, 59),
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+    );
+  }
+}
