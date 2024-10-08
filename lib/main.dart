74bfcdb2c0009173e886dec95960b947916e4ef5
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-08 15:36:36 +0200

    a

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Text('Hello World'),
+        title: const Text('My First App'),
       ),
       body: const Center(
         child: Text('Hello World'),
